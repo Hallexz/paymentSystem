e19b12d3d7785c10b2b5482f41685df053ed9a0b
Hallex <136874674+Hallexz@users.noreply.github.com>
2024-05-04 13:53:38 +0700

    Update main.go

--- main.go
+++ main.go
@@ -17,18 +17,14 @@ func main() {
 	s := src.Server{}
 	ps := src.PaymentService{}
 
-	// Создание экземпляра TransactionServer
 	ts := &src.TransactionServer{
 		Transactions: make(map[string][]*pb.Transaction),
 	}
 
-	// Создание экземпляра UserServiceServer
 	us := src.NewUserServiceServer()
 
-	// Создание экземпляра Bank
 	bank := src.NewBank()
 
-	// Создание нового банковского счета
 	newAccount := &pb.BankAccount{
 		User:          "John Doe",
 		AccountNumber: "123456789",
@@ -38,25 +34,21 @@ func main() {
 		CardCVV:       "123",
 	}
 
-	// Добавление нового банковского счета
 	response := bank.AddBankAccount(newAccount)
 	fmt.Println(response.Confirmation)
 
-	// Запуск сервера
+
 	lis, err := net.Listen("tcp", "localhost:50051")
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
 
-	// Регистрация серверов
 	pb.RegisterPaymentServiceServer(grpcServer, &s)
 	pb.RegisterPaymentServiceServer(grpcServer, &ps)
 
-	// Регистрация TransactionServer
 	pb.RegisterTransactionServiceServer(grpcServer, ts)
 
-	// Регистрация UserServiceServer
 	pb.RegisterUserServiceServer(grpcServer, us)
 
 	if err := grpcServer.Serve(lis); err != nil {
